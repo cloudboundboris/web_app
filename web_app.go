d0aab99cb168f661d7758e58b9536c34acac996e
Boris Levenzon <blevenzon@gmail.com>
2023-05-29 20:05:12 -0400

    created web app for github

---
+++ web_app.go
@@ -0,0 +1,12 @@
+package main
+import (
+	"fmt"
+	"net/http"
+)
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello!")
+})
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+	panic(err)
