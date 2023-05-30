3321fccbc26d13b5d6c8c9d9705b3e82b1cad1a9
Boris Levenzon <blevenzon@gmail.com>
2023-05-29 20:29:55 -0400

    was missing { in the main program

--- web_app.go
+++ web_app.go
@@ -5,8 +5,13 @@ import (
 )
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello!")
-})
+		fmt.Fprint(w, "Hello!")
+	
+	})
 	err := http.ListenAndServe("0.0.0.0:80", nil)
 	if err != nil {
-	panic(err)
+	       panic(err)
+	}
+
+}
+
