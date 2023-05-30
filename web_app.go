42de4d5855a0823271b93bf4882dff1ba2d2344b
Boris Levenzon <blevenzon@gmail.com>
2023-05-29 21:15:06 -0400

    Update web_app.go

--- web_app.go
+++ web_app.go
@@ -5,7 +5,7 @@ import (
 )
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello!!***")
+		fmt.Fprint(w, "Hello!!!!!!!!!")
 	
 	})
 	err := http.ListenAndServe("0.0.0.0:80", nil)
